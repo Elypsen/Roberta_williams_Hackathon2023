c2ab58f091056bb8cad37812490657f8d9591461
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-12 10:46:05 +0200

    squelette du projet

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,14 +5,11 @@ export function Footer() {
             <div className='sm:flex sm:items-center sm:justify-between'>
                <p className='mb-4 flex items-center sm:mb-0'>
                   <span className='self-center whitespace-nowrap text-2xl font-semibold dark:text-white'>
-                     TodoList
+                     Mon Festoche
                   </span>
                </p>
                <ul
                   className='mb-6 flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mb-0 space-x-4 md:space-x-6'>
-                  <li>
-                     <p className='hover:underline hover:cursor-not-allowed'>About</p>
-                  </li>
                   <li>
                      <p className='hover:underline hover:cursor-not-allowed'>Privacy Policy</p>
                   </li>
@@ -28,11 +25,14 @@ export function Footer() {
                </ul>
             </div>
             <hr className='my-6 border-gray-700 sm:mx-auto lg:my-8' />
-            <span className='block text-sm text-gray-500 dark:text-gray-400 sm:text-center'>
-               © 2023 <p className='hover:underline'>TodoList™</p> Y-Y-M Team All Rights Reserved.
-            </span>
+            <p className='block text-sm text-gray-500 dark:text-gray-400 sm:text-center'>
+               © 2023 <span className='hover:underline'>Hackathon™</span>
+            </p>
+            <p className='block text-sm text-gray-500 dark:text-gray-400 sm:text-center'>
+               Team Roberta Williams All Rights Reserved.
+            </p>
          </div>
       </footer>
-   );
+   )
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,41 +1,18 @@
 export default function Header() {
-   // const isLoggedIn = useAuthStore(state => state.isLoggedIn)
-   // const logout = useAuthStore(state => state.logout)
-   // const {pathname} = useLocation()
-   return (
-      <header>
-         <nav className=" border-base-300 bg-base-200 w-screen px-4 py-2.5 lg:px-6">
-            <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-               <h1 className="self-center whitespace-nowrap text-xl font-semibold">TodoList</h1>
-               <nav className="flex items-center lg:order-2">
-                  {/*{isLoggedIn ? (*/}
-                  {/*   <>*/}
-                  {/*      <NavigationLink*/}
-                  {/*         active={pathname === ROUTES.index}*/}
-                  {/*         as={Link}*/}
-                  {/*         to={ROUTES.index}>*/}
-                  {/*         Home*/}
-                  {/*      </NavigationLink>*/}
-                  {/*      <NavigationLink*/}
-                  {/*         as={'button'}*/}
-                  {/*         onClick={logout}>*/}
-                  {/*         Log out*/}
-                  {/*      </NavigationLink>*/}
-                  {/*   </>*/}
-                  {/*) : (*/}
-                  {/*   <>*/}
-                  {/*      <NavigationLink*/}
-                  {/*         active={pathname === ROUTES.auth}*/}
-                  {/*         as={Link}*/}
-                  {/*         to={ROUTES.auth}>*/}
-                  {/*         Log in*/}
-                  {/*      </NavigationLink>*/}
-                  {/*   </>*/}
-                  {/*)}*/}
-               </nav>
-            </div>
-         </nav>
-      </header>
-   )
+    // const isLoggedIn = useAuthStore(state => state.isLoggedIn)
+    // const logout = useAuthStore(state => state.logout)
+    // const {pathname} = useLocation()
+    return (
+        <header>
+            <nav className="bg-primary w-screen px-4 py-2.5 lg:px-6">
+                <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
+                    <h1 className="text-xl text-primary-content">Mon Festoche</h1>
+                    <nav className="flex items-center lg:order-2">
+
+                    </nav>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
