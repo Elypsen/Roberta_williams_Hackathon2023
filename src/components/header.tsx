76d0bae346c503f6e2f481c96d39be4c16e8e3af
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-12 11:37:24 +0200

    Hero banner : add picture, card, cta button, and animations

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,18 @@
 export default function Header() {
-    // const isLoggedIn = useAuthStore(state => state.isLoggedIn)
-    // const logout = useAuthStore(state => state.logout)
-    // const {pathname} = useLocation()
-    return (
-        <header>
-            <nav className="bg-primary w-screen px-4 py-2.5 lg:px-6">
-                <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-                    <h1 className="text-xl text-primary-content">Mon Festoche</h1>
-                    <nav className="flex items-center lg:order-2">
+   // const isLoggedIn = useAuthStore(state => state.isLoggedIn)
+   // const logout = useAuthStore(state => state.logout)
+   // const {pathname} = useLocation()
+   return (
+      <header>
+         <nav className='bg-primary-content w-screen px-4 py-2.5 lg:px-6'>
+            <div className='mx-auto flex max-w-screen-xl flex-wrap items-center justify-between'>
+               <p className='text-xl text-base-content font-inter uppercase'>Mon Festoche</p>
+               <nav className='flex items-center lg:order-2'>
 
-                    </nav>
-                </div>
-            </nav>
-        </header>
-    )
+               </nav>
+            </div>
+         </nav>
+      </header>
+   )
 }
 
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,34 +1,18 @@
+import HeroBanner from '../components/heroBanner'
+import ResultSection from '../components/resultSection'
+
 export default function HomePage() {
 
 
    return (
       <main className={'min-h-screen w-screen '}>
          {/*HERO*/}
-         <section className={'relative flex justify-center items-center flex-col w-full h-screen'}><h1
-            className={'sr-only'}>Mon Festoche</h1>
-            <div className={'absolute t-0 l-0 h-full w-full bg-gray-500 animate-pulse opacity-50'}></div>
-            <a href='#results'
-               className='relative rounded px-5 py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300'>
-               <span
-                  className='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease'></span>
-               <span className='relative'>Button Text</span>
-            </a>
-         </section>
+         <HeroBanner />
          {/*RESULTS*/}
-         <section id={'results'} className={'w-full min-h-48'}>
-            <div className='relative form-control w-full max-w-xs border'>
-               <label className='label'>
-                  <span className='label-text'>What is your name?</span>
-               </label>
-               <input type='text' placeholder='Type here' className='input input-bordered w-full max-w-xs' />
-               <label className='label'>
-                  <span className='label-text-alt'>Some message feedback to user...</span>
-               </label>
-            </div>
-            <h2>RESULTATS</h2>
+         <ResultSection />
 
-         </section>
       </main>
    )
 }
+
 
