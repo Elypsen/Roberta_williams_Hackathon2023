dbd2b77470403fff89fd70828e77883da67f147e
Elypsen <bacon.yann@gmail.com>
2023-10-12 15:14:30 +0200

    services create + getAll

--- src/components/pages/home.page.tsx
+++ src/components/pages/home.page.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import HeroBanner from "../sections/heroBanner";
 import ResultSection from "../sections/resultSection";
+import { getAllFestivals } from "../../services/api.service";
 
 
 export default function HomePage() {
-
+   useEffect(() => {
+      getAllFestivals().then((result) => console.log("useeffect:", result) ).catch(err=> console.log(err)); 
+   })
    return (
       <main className={'min-h-screen w-screen '}>
          {/*HERO*/}
---
+++ src/services/api.service.ts
@@ -0,0 +1,18 @@
+import ky from 'ky';
+import { TUNNEL } from '../TUNNEL';
+const getErrorMessage = (error: unknown) => {
+    if (error instanceof Error) return error.message;
+    return String(error);
+  };
+const api = ky.create({
+    prefixUrl:TUNNEL
+})
+
+export const getAllFestivals = async() => {
+    try{
+        const festivals = await api.get('').json();
+        return festivals;
+    }catch(err){
+        console.log(getErrorMessage(err));
+    }
+}
